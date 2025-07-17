bff3d5d34df12d21e61ea64c81eb48ddcd9814de
ErebosXYZ <robertmaynou@gmail.com>
2025-07-17 21:32:58 +0200

    done until bonus 2

--- script.js
+++ script.js
@@ -14,3 +14,106 @@ const ciudades = {
 
 // ¡Buena suerte!
 
+
+
+const citySelector = document.getElementById('citySelector');
+const cityInfo = document.getElementById('cityInfo');
+const cityName = document.getElementById('cityNombre');
+const cityImage = document.getElementById('cityImagen');
+const cityDescription = document.getElementById('cityDescripcion');
+
+
+// ORIGINAL VERSION
+// citySelector.addEventListener('change', (event) => {
+//     const city = event.target.value;
+//     cityInfo.style.display = city !== "" ? block : none;
+//     if (city === "palamos") {
+//         cityName.textContent = ciudades.palamos.nombre;
+//         cityImage.src = ciudades.palamos.imagen;
+//         cityDescription.textContent = ciudades.palamos.descripcion;
+//     } else if (city === "blanes") {
+//         cityName.textContent = ciudades.blanes.nombre;
+//         cityImage.src = ciudades.blanes.imagen;
+//         cityDescription.textContent = ciudades.blanes.descripcion;
+
+//     }  
+
+// });
+
+
+//  * SHORTER VERSION
+
+citySelector.addEventListener('change', async (event) => {
+    const city = event.target.value;
+
+    //   Si city no és igual a un string buit fem un display block, en cas contrari fem un display none
+    cityInfo.style.display = city !== "" ? 'block' : 'none';
+
+    // Si la city (el valor que extreiem de l'event change) de l'objecte ciudades forma part de les dades de l'objecte, omplim el camp corresponent amb la propietat corresponent
+    if (ciudades[city]) {
+        const data = ciudades[city];
+        cityName.textContent = data.nombre;
+        cityImage.src = data.imagen;
+        cityDescription.textContent = data.descripcion;
+    }
+
+    const url = `https://bravabook.onrender.com/api/apartments/search?city=${city}`
+
+    try {
+        const response = await fetch(url);
+        // Gestió d'errors de la resposta
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+    
+        const accomodationsInfo = document.getElementById('accomodationsInfo');
+        const numAccomodations = document.getElementById('numAccomodations');
+        const accomodations = document.getElementById('accomodations');
+
+        // Assegurem que accomodations es reiniciï sempre (opcional)
+        accomodations.innerHTML = "";
+
+        // Si hi ha més de 0 objectes dins de data, mostrem el contenidor i el text amb la quantitat 
+        if (data.length > 0) {
+            accomodationsInfo.style.display = "block";
+            numAccomodations.textContent = data.length;
+        } else {
+            accomodationsInfo.style.display = "none";
+            return;
+        }
+        // Recorrem l'array d'objectes que és data amb el mètode forEach, igual que a la pràctica bravabook(kihome booking)
+        
+        data.forEach( apartment => {
+            // Creem un div per a cada element de l'array
+            const card = document.createElement('div');
+
+            // Aquest div conté un enllaç que ens envia a l'endpoint de l'apartament a través de la id del seu objecte. si afegim # i la id d'un element html concret de l'endpoint on accedim, ens redirigirà automàticament allà.
+            card.innerHTML = `
+            <h2><a href=https://bravabook.onrender.com/apartment/${apartment._id}#reservation>${apartment.title}</a></h2>
+            <img src=${apartment.mainPhoto}/>
+            <p>Precio por noche: <span class=apartment-price>${apartment.price}€</span>/p>
+            `
+
+            // Appendchild agrega un fill al node del DOM que estem manipulant, en aquest cas afegeix un div card a accomodations.
+            accomodations.appendChild(card);
+
+            // Canviem el color del preu depenent de si costa menys de 100€ (verd) o més de 200€ per nit.
+            if (apartment.price < 100) {
+                card.querySelector('.apartment-price').style.color = "green";
+            } if (apartment.price > 200) {
+                card.querySelector('.apartment-price').style.color = "red";
+            }
+        });
+        
+    }
+    catch (error) {
+        console.error('Hubo un problema con el fetch')
+    }
+});
+
+
